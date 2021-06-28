96e9c5f3193da7c3147e9d267e4f5ebb6a721a1f
Nikolai Glebanov <n.glebanov@gmail.com>
2021-06-28 11:00:48 +0300

    Добавлена страница сотрудников, и вторая страница

--- InternetShop/Startup.cs
+++ InternetShop/Startup.cs
@@ -20,13 +20,13 @@ namespace InternetShop
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
+        
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews().AddRazorRuntimeCompilation();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -36,7 +36,7 @@ namespace InternetShop
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+               
                 app.UseHsts();
             }
             app.UseHttpsRedirection();
